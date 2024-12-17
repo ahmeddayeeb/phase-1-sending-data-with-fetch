dd40ad21bcf93d87d3f7954e1639016885aae5d2
ahmed <ahmeddayeeb@gmail.com>
2024-12-17 15:22:10 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email) {
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      const id = object.id;
+      document.body.innerHTML += `<p>New ID: ${id}</p>`;
+    })
+    .catch(function (error) {
+      document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+    });
+}
